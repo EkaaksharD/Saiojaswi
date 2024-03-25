b3d26fbf70033f6f2ed91e1cd2efb210c56429cc
EkaaksharD <ekaaksharojaswi.d@gmail.com>
2024-03-24 21:00:08 -0400

    first commit

---
+++ FacebookLogin.java
@@ -0,0 +1,34 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class FacebookLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		ChromeOptions popupeliminater = new ChromeOptions();
+		popupeliminater.addArguments("--disable-notifications");
+		
+		WebDriver driver = new ChromeDriver(popupeliminater);
+		
+		
+		
+		//To invoke a browser 
+		driver.get("https://www.facebook.com/");
+		// To open Facebook
+
+		System.out.println(driver.getTitle());
+		// To Print the Title of the page
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		//By using Implicit wait , asking the browser to wait for 5 sec
+	}
+
+}
